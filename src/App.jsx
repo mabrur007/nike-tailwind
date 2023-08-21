68f7f8bed486ebfed5ec40f963ad3396766deb9a
Mir Md Mabrur <96931879+mabrur007@users.noreply.github.com>
2023-08-21 13:03:05 +0600

    fav icon changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold text-center text-stone-600">
-      Hello world!
-    </h1>
+    <div className="max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold text-center text-stone-600">
+        Hello world!
+      </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, illum itaque, omnis veniam deserunt iste ex, fugit atque architecto ratione temporibus autem aspernatur! Non veniam magnam iure dolore sequi nihil, qui nam inventore eligendi. Architecto consequuntur cum similique. Sunt id magni non blanditiis hic expedita distinctio quas quia ad? Alias?</p>
+    </div>
   )
 }
