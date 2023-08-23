ed4a2d954f992e704cfc73fcd89dda6356830304
Mir Md Mabrur <96931879+mabrur007@users.noreply.github.com>
2023-08-23 12:19:41 +0600

    nav Links and hamburger icon added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,6 @@
+import { hamburger } from "../assets/icons"
 import { headerLogo } from "../assets/images"
+import { navLinks } from "../constants"
 
 const Nav = () => {
   return (
@@ -7,9 +9,18 @@ const Nav = () => {
             <a href="/">
                 <img src={headerLogo} alt="header_logo" width={130} height={30}/>
             </a>
-            <ul className="">
-
+            <ul className="flex flex-1 justify-center items-center gap-16 max-lg:hidden">
+              {navLinks.map((item) => (
+                <li key={item.label}>
+                  <a href={item.href}>
+                    {item.label}
+                  </a>
+                </li>
+              ))}
             </ul>
+            <div className="lg:hidden">
+              <img src={hamburger} alt="hamburger" width={25} height={25} />
+            </div>
         </nav>
     </header>
   )
